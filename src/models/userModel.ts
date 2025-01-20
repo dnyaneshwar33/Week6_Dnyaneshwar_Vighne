c17c123f0d533b276706d8f1cb539f94cc4a79a2
Dnyaneshwar Vighne <125205721+dnyaneshwar33@users.noreply.github.com>
2025-01-20 21:50:31 +0530

    Update userModel.ts

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -35,6 +35,10 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    mobile: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+    }
     email: {
       type: DataTypes.STRING,
       allowNull: false,
